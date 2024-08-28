e48d31a15768db694521b091a52c65d5540b7f9e
PJ-004 <pranavjworkacc@gmail.com>
2024-08-27 20:29:08 -0700

    Fixed Desktop Applications

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if [[ "$main.cpp" && "$settings.h" ]]; then
 	echo "Installing..."
 	g++ $(pkg-config --cflags --libs Qt6Widgets qtermwidget6) -fPIC -o PJTerm main.cpp
 	sudo cp PJTerm /usr/local/bin
-	sudo cp PJTerm.desktop /usr/share/xsessions
+	sudo cp PJTerm.desktop ~/.local/share/applications
 	echo "Installation Complete"
 else
 	echo "ERROR: Not all files are present"
